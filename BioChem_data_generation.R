5e756933752704e507409cf71a31a1e470d3503c
EOGrady21 <38440373+EOGrady21@users.noreply.github.com>
2024-04-25 14:05:06 -0300

    working on filling in bcd generation

--- BioChem_data_generation.R
+++ BioChem_data_generation.R
@@ -39,53 +39,80 @@ if (length(missing_cols) > 0) {
 # Use rename_with to rename the columns
 data <- data %>% rename_with(~ name_changes[.x], everything())
 
+# gather metadata columns
+bcd_meta <- data[names(data) %in% bcd_dict$biochem[bcd_dict$tag == 'metadata']]
+
 # generate necessary columns ----
 
 # DIS_SAMPLE_KEY_VALUE
 # (mission_event_sampleID)
 
 # check mission name - adjust to expocode
-if (unique(data$MISSION_NAME) %in% expos$cruise_name) {
-  data$MISSION_NAME <- expos$expocode[expos$cruise_name == unique(data$MISSION_NAME)]
+if (unique(bcd_meta$MISSION_NAME) %in% expos$cruise_name) {
+  bcd_meta$MISSION_NAME <- expos$expocode[expos$cruise_name == unique(bcd_meta$MISSION_NAME)]
 } else{
   warning('EXPOCODE not found, MISSION_NAME was left in original condition! Please add an entry to extdata/expocodes.csv')
 }
 
 # pad event number to three digits
-data$COLLECTOR_EVENT_ID <- str_pad(data$COLLECTOR_EVENT_ID, width = 3, pad = '0')
+bcd_meta$COLLECTOR_EVENT_ID <- str_pad(bcd_meta$COLLECTOR_EVENT_ID, width = 3, pad = '0')
 
 # generate dis_sample_key_value
-data <- data %>%
+bcd_meta <- bcd_meta %>%
   dplyr::mutate(., DIS_SAMPLE_KEY_VALUE = paste0(MISSION_NAME, '_', COLLECTOR_EVENT_ID, '_', COLLECTOR_SAMPLE_ID))
 
 
 # EVENT_SDATE
 
-data$START_DATE <- make_date(data$year, data$month, data$day)
+bcd_meta$START_DATE <- make_date(bcd_meta$year, bcd_meta$month, bcd_meta$day)
 
 # Convert the date to the format DD-MON-YY
-data$START_DATE <- format(data$START_DATE, "%d-%b-%y")
+bcd_meta$START_DATE <- format(bcd_meta$START_DATE, "%d-%b-%y")
+
+# remove year month day columns
+bcd_meta <- bcd_meta[-grep(names(bcd_meta), pattern = 'year')]
+bcd_meta <- bcd_meta[-grep(names(bcd_meta), pattern = 'month')]
+bcd_meta <- bcd_meta[-grep(names(bcd_meta), pattern = 'day')]
 
 # CREATED_DATE
-data$CREATED_DATE <- format(Sys.Date(), "%d-%b-%y")
-
-# reformat to long data (with flags)
-
-# DIS_HEADR_GEAR_SEQ
-
-
+bcd_meta$CREATED_DATE <- format(Sys.Date(), "%d-%b-%y")
 
 # fill in standard values
-# MISSION_INSTITUTE = DFO BIO
-# CREATED_BY = EMILY OGRADY
-# DATA_CENTER_CODE = 20
-# PROCESS_FLAG = NR
 
+# MISSION_INSTITUTE = DFO BIO
+bcd_meta$MISSION_INSTITUTE <- 'DFO-BIO'
+# CREATED_BY = EMILY OGRADY
+bcd_meta$CREATED_BY <- "Emily O'Grady"
+# DATA_CENTER_CODE = 20
+bcd_meta$DATA_CENTER_CODE <- '20'
+# PROCESS_FLAG = NR
+bcd_meta$PROCESS_FLAG <- 'NR'
+
+
+# reformat to long data (with flags) ----
+
+bcd_data <- data[names(data) %in% bcd_dict$biochem[bcd_dict$tag != 'metadata']]
+
+bcd_wide <- cbind(bcd_meta, bcd_data)
+
+data_cols <- names(bcd_wide)[which(names(bcd_wide) %in% bcd_dict$biochem[bcd_dict$tag %in% c('btl', 'ctd')])]
+qc_cols <- names(bcd_wide)[which(names(bcd_wide) %in% bcd_dict$biochem[bcd_dict$tag == 'qc'])]
+
+# make all data columns numeric
+bcd_wide <- bcd_wide %>%
+  mutate_at(vars(data_cols), as.numeric)
+
+bcd_long <- bcd_wide %>%
+  pivot_longer(., all_of(data_cols), names_to = 'METHOD', values_to = 'DATA_VALUE') %>%
+  pivot_longer()
 # pull out QC columns
 
 
 
-# translate METHOD
+# DIS_HEADR_GEAR_SEQ
+
+# Depth?
+
 
 # translate flags
 
