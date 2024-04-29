42330610af6be92a1280ebf8f141199e511a9d15
EOGrady21 <38440373+EOGrady21@users.noreply.github.com>
2024-04-29 14:21:06 -0300

    BCS/BCD generation scripts completed

--- BioChem_data_generation.R
+++ BioChem_data_generation.R
@@ -1,19 +1,19 @@
 # Translate Davis Strait data to BioChem BCD/BCS format
 library(librarian)
-shelf(tidyverse, readr, readxl, here)
-source('functions.R')
+shelf(tidyverse, readr, readxl, here, oce )
 
 
 # read in dictionary tables
-bcs_dict <- readxl::read_xlsx(here('extdata', 'biochem_dictionary.xlsx'),
-                              sheet = 'BCS')
 bcd_dict <- readxl::read_xlsx(here('extdata', 'biochem_dictionary.xlsx'),
                               sheet = 'BCD')
 expos <- read_csv('extdata/expocodes.csv', show_col_types = FALSE)
+flags <- read_xlsx(here('extdata', 'flag_dictionary.xlsx'))
+flag_definitions <- read_csv('extdata/flags.csv', show_col_types = FALSE)
+data_type_seq_table <- read_csv('extdata/biochem_data_types.csv', show_col_types = FALSE)
 
 # read in original data
-# TODO make this more reproducible, cleaner, maybe file select
-data_fns <- list.files('../data/', pattern = '_EO.csv', full.names = TRUE)
+data_fns <- choose.files(default = '../data', caption = "Select data files: ", multi = TRUE)
+#data_fns <- list.files('../data/', pattern = '_EO.csv', full.names = TRUE)
 
 
 # BCD ----
@@ -22,6 +22,8 @@ for (i in 1:length(data_fns)) {
   
 data <- read_csv(data_fns[i], show_col_types = FALSE)
 # TODO check for empty columns before translating
+
+
 
 # translate columns ----
 bcd_dict_y <- bcd_dict[bcd_dict$original %in% names(data),]
@@ -42,6 +44,9 @@ if (length(missing_cols) > 0) {
 # Use rename_with to rename the columns
 data <- data %>% rename_with(~ name_changes[.x], everything())
 
+# remove empty rows
+data <- data[-which(is.na(data$MISSION_NAME)), ]
+
 # gather metadata columns
 bcd_meta <- data[names(data) %in% bcd_dict$biochem[bcd_dict$tag == 'metadata']]
 
@@ -53,29 +58,35 @@ bcd_meta <- data[names(data) %in% bcd_dict$biochem[bcd_dict$tag == 'metadata']]
 # check mission name - adjust to expocode
 if (unique(bcd_meta$MISSION_NAME) %in% expos$cruise_name) {
   bcd_meta$MISSION_NAME <- expos$expocode[expos$cruise_name == unique(bcd_meta$MISSION_NAME)]
+  bcd_meta$MISSION_DESCRIPTOR <- expos$MEDS[expos$expocode == unique(bcd_meta$MISSION_NAME)]
+  
 } else{
-  warning('EXPOCODE not found, MISSION_NAME was left in original condition! \n
+  stop('EXPOCODE not found, MISSION_NAME was left in original condition! \n
+          No Mission Descriptor provided! \n
           Please add an entry to extdata/expocodes.csv')
 }
 
 # pad event number to three digits
-bcd_meta$COLLECTOR_EVENT_ID <- str_pad(bcd_meta$COLLECTOR_EVENT_ID,
+bcd_meta$EVENT_COLLECTOR_EVENT_ID <- str_pad(bcd_meta$EVENT_COLLECTOR_EVENT_ID,
                                        width = 3,
                                        pad = '0')
 
 # generate dis_sample_key_value
 bcd_meta <- bcd_meta %>%
   dplyr::mutate(., DIS_SAMPLE_KEY_VALUE = paste0(
-    MISSION_NAME, '_', COLLECTOR_EVENT_ID, '_', COLLECTOR_SAMPLE_ID
+    MISSION_NAME, '_', EVENT_COLLECTOR_EVENT_ID, '_', DIS_DETAIL_COLLECTOR_SAMP_ID
     ))
 
 
 # EVENT_SDATE
 
-bcd_meta$START_DATE <- make_date(bcd_meta$year, bcd_meta$month, bcd_meta$day)
+bcd_meta$DIS_HEADER_SDATE <- make_date(bcd_meta$year, bcd_meta$month, bcd_meta$day)
 
 # Convert the date to the format DD-MON-YY
-bcd_meta$START_DATE <- format(bcd_meta$START_DATE, "%d-%b-%y")
+bcd_meta$DIS_HEADER_SDATE <- format(bcd_meta$DIS_HEADER_SDATE, "%d-%b-%y")
+
+# format time TODO Check format
+bcd_meta$DIS_HEADER_STIME <- format(bcd_meta$DIS_HEADER_STIME, "%H%m")
 
 # remove year month day columns
 bcd_meta <- bcd_meta[-grep(names(bcd_meta), pattern = 'year')]
@@ -105,9 +116,6 @@ bcd_data <- data[names(data) %in% bcd_dict$biochem[bcd_dict$tag != 'metadata']]
 
 bcd_wide <- cbind(bcd_meta, bcd_data)
 
-# make all data columns numeric
-bcd_wide <- bcd_wide %>%
-  mutate_at(vars(data_cols), as.numeric)
 
 # separate column types
 data_cols <- names(bcd_wide)[
@@ -119,6 +127,10 @@ qc_cols <- names(bcd_wide)[
 metadata_cols <- names(bcd_wide)[
   -which(names(bcd_wide) %in% bcd_dict$biochem[bcd_dict$tag %in% c('ctd', 'btl', 'qc')])
   ]
+
+# make all data columns numeric
+bcd_wide <- bcd_wide %>%
+  mutate_at(vars(data_cols), as.numeric)
 
 
 # Separate data and qc columns into two dataframes
@@ -140,27 +152,89 @@ qc_long$DATA_TYPE_METHOD <- str_remove(qc_long$DATA_TYPE_METHOD, "_qc")
 bcd_long <- left_join(data_long, qc_long, by = c("DATA_TYPE_METHOD",
                                                  as.character(metadata_cols)))
 
-
+# remove any NA data rows
+bcd_long <- bcd_long %>%
+  dplyr::filter(!is.na(DIS_DETAIL_DATA_VALUE))
 # Add more metadata in long format ----
 
 # Depth
 # calculate from pressure
+pressure_data <- bcd_long %>%
+  filter(., DATA_TYPE_METHOD == 'Pressure') %>%
+  select(., DIS_SAMPLE_KEY_VALUE, DIS_DETAIL_DATA_VALUE)
+pressure_data$DIS_HEADER_START_DEPTH <- oce::swDepth(pressure_data$DIS_DETAIL_DATA_VALUE,
+                           latitude = bcd_long$DIS_HEADER_SLON[1])
+pressure_data$DIS_HEADER_END_DEPTH <- oce::swDepth(pressure_data$DIS_DETAIL_DATA_VALUE,
+                                                     latitude = bcd_long$DIS_HEADER_SLON[1])
+pressure_data <- pressure_data %>%
+  select(-DIS_DETAIL_DATA_VALUE) %>%
+  distinct(., DIS_SAMPLE_KEY_VALUE, .keep_all = TRUE)
+
+# join depth data 
+bcd_final <- bcd_long %>%
+  dplyr::left_join(pressure_data)
+
+
+# clean up
+remove(pressure_data, data_long, qc_long, data_df, qc_df)
+
 
 # get dis_data_type_seq
-# gather from biochem tables
+for (ii in 1:length(bcd_final$DATA_TYPE_METHOD)) {
+  bcd_final$DIS_DETAIL_DATA_TYPE_SEQ[ii] <- as.character(data_type_seq_table$DATA_TYPE_SEQ[data_type_seq_table$METHOD == bcd_final$DATA_TYPE_METHOD[ii]])
+}
 
-# add DIS_DATA_NUM (sequentially)
-
-
+# add DIS_DATA_NUM 
+bcd_final$DIS_DATA_NUM <- seq(1:length(bcd_final$MISSION_NAME))
 
 # translate flags ----
-
-# add base flags (1s, 9s)
-
-# pull any notes into MISSION_COLLECTOR_COMMENT1
+for (ii in 1:length(bcd_final$DIS_DETAIL_DATA_QC_CODE)) {
+  lab_qc <- bcd_final$DIS_DETAIL_DATA_QC_CODE[ii]
+  
+  if (!is.na(lab_qc) && nchar(lab_qc) < 4) { # grab biochem flag from dictionary if lab flag exists
+    bc_qc <- flags$BioChem[grep(flags$lab, pattern = str_sub(lab_qc, 1, 2))]
+  } else {
+    if (is.na(lab_qc)) { # if no flag then assume acceptable
+      bc_qc <- 1
+    } else if (nchar(lab_qc) > 4) { # if there is text ask for user translation
+      bc_qc <- menu(choices = flag_definitions$biochem_definition[flag_definitions$flag > 0 ], 
+            title = paste("Translate this QC to BioChem flag: \n", lab_qc))
+    }
+  }
+  bcd_final$DIS_DETAIL_DATA_QC_CODE[ii] <- bc_qc
+}
 
 # order columns
+bcd_columns <- c('DIS_DATA_NUM',
+                 'MISSION_DESCRIPTOR',
+                 'MISSION_NAME',
+                 'EVENT_COLLECTOR_EVENT_ID',
+                 'EVENT_COLLECTOR_STN_NAME',
+                 'DIS_HEADER_START_DEPTH',
+                 'DIS_HEADER_END_DEPTH',
+                 'DIS_HEADER_SLAT',
+                 'DIS_HEADER_SLON',
+                 'DIS_HEADER_SDATE',
+                 'DIS_HEADER_STIME',
+                 'DIS_DETAIL_DATA_TYPE_SEQ',
+                 'DATA_TYPE_METHOD',
+                 'DIS_DETAIL_DATA_VALUE',
+                 'DIS_DETAIL_DATA_QC_CODE',
+                 'DIS_DETAIL_COLLECTOR_SAMP_ID',
+                 'MISSION_INSTITUTE',
+                 'CREATED_BY',
+                 'CREATED_DATE',
+                 'DATA_CENTER_CODE',
+                 'PROCESS_FLAG',
+                 'BATCH_SEQ',
+                 'DIS_SAMPLE_KEY_VALUE'
+)
 
+bcd_export <- bcd_final %>%
+  select(all_of(bcd_columns))
 # export data file
+bcd_name <- file.path('../data/BioChem/', paste0(unique(bcd_export$MISSION_DESCRIPTOR), '_BCD.csv'))
+write.csv(bcd_export, bcd_name, row.names = FALSE)
 
 }
+
--- BioChem_metadata_generation.R
+++ BioChem_metadata_generation.R
@@ -1,31 +1,177 @@
 # Generate BCS files
+library(librarian)
+shelf(tidyverse, readr, readxl, here, oce )
 
-# read in dictionary table
+# read in dictionary tables ----
 
 # gather from OSC cruise
+oscdata <- read_xlsx('osccruise_davisstrait.xlsx')
+
+# dependency tables
+bcs_dict <- readxl::read_xlsx(here('extdata', 'biochem_dictionary.xlsx'),
+                              sheet = 'BCS')
+bcd_dict <- readxl::read_xlsx(here('extdata', 'biochem_dictionary.xlsx'),
+                              sheet = 'BCD')
+
+bcs_columns <- c('DIS_SAMPLE_KEY_VALUE',
+                 'MISSION_DESCRIPTOR',
+                 'EVENT_COLLECTOR_EVENT_ID',
+                 'EVENT_COLLECTOR_STN_NAME',
+                 'MISSION_NAME',
+                 'MISSION_LEADER',
+                 'MISSION_SDATE',
+                 'MISSION_EDATE',
+                 'MISSION_INSTITUTE',
+                 'MISSION_PLATFORM',
+                 'MISSION_PROTOCOL',
+                 'MISSION_GEOGRAPHIC_REGION',
+                 'MISSION_COLLECTOR_COMMENT1',
+                 'MISSION_COLLECTOR_COMMENT2',
+                 'MISSION_DATA_MANAGER_COMMENT',
+                 'EVENT_SDATE',
+                 'EVENT_EDATE',
+                 'EVENT_STIME',
+                 'EVENT_ETIME',
+                 'EVENT_MIN_LAT',
+                 'EVENT_MAX_LAT',
+                 'EVENT_MIN_LON',
+                 'EVENT_MAX_LON',
+                 'EVENT_UTC_OFFSET',
+                 'EVENT_COLLECTOR_COMMENT1',
+                 'EVENT_COLLECTOR_COMMENT2',
+                 'EVENT_DATA_MANAGER_COMMENT',
+                 'DIS_HEADR_GEAR_SEQ',
+                 'DIS_HEADR_SDATE',
+                 'DIS_HEADR_EDATE',
+                 'DIS_HEADR_STIME',
+                 'DIS_HEADR_ETIME',
+                 'DIS_HEADR_TIME_QC_CODE',
+                 'DIS_HEADR_SLAT',
+                 'DIS_HEADR_ELAT',
+                 'DIS_HEADR_SLON',
+                 'DIS_HEADR_ELON',
+                 'DIS_HEADR_POSITION_QC_CODE',
+                 'DIS_HEADR_START_DEPTH',
+                 'DIS_HEADR_END_DEPTH',
+                 'DIS_HEADR_SOUNDING',
+                 'DIS_HEADR_COLLECTOR_DEPLMT_ID',
+                 'DIS_HEADR_COLLECTOR_SAMPLE_ID',
+                 'DIS_HEADR_COLLECTOR',
+                 'DIS_HEADR_COLLECTOR_COMMENT1',
+                 'DIS_HEADR_DATA_MANAGER_COMMENT',
+                 'DIS_HEADR_RESPONSIBLE_GROUP',
+                 'DIS_HEADR_SHARED_DATA',
+                 'CREATED_BY',
+                 'CREATED_DATE',
+                 'DATA_CENTER_CODE',
+                 'PROCESS_FLAG',
+                 'BATCH_SEQ' )
 
 # read in all original data sources
 # data
-# metadata
-# dependency tables
+data_fn <- choose.files(default = '../data', caption = "Select data files: ", multi = TRUE)
+raw_data <- read.csv(data_fn)
+# remove empty rows
+# raw_data <- raw_data[-which(is.na(raw_data[,1])), ]
 
-# generate columns
-# DIS_HEADR_GEAR_SEQ
-# generate based on dictionary tag?
-btl_type <- bcd_dict$biochem[bcd_dict$tag == 'btl']
-ctd_type <- bcd_dict$biochem[bcd_dict$tag == 'ctd']
-
-btl_gear_seq <- '90000019'
-ctd_gear_seq <- '90000171'
-
-bcd_long <- bcd_long %>%
-  dplyr::mutate(., case_when())
+# bcd
+bcd_fn <- choose.files(default = '../data/BioChem',
+                       #caption = paste('Select BCD file for', unique(raw_data$`Cruise ID`), '[', unique(raw_data$year), ']', ':')
+                       )
+bcd <- read.csv(bcd_fn)
 
 
-# fill standard values
+# pull columns from bcd ----
 
-# pull values from existing tables
+bcs <- bcd %>%
+  dplyr::select(., DIS_SAMPLE_KEY_VALUE,
+                MISSION_DESCRIPTOR,
+                EVENT_COLLECTOR_EVENT_ID,
+                EVENT_COLLECTOR_STN_NAME,
+                MISSION_NAME,
+                MISSION_INSTITUTE,
+                CREATED_BY,
+                CREATED_DATE,
+                DATA_CENTER_CODE,
+                PROCESS_FLAG,
+                BATCH_SEQ
+                )
 
-# format
+bcs$DIS_HEADR_SDATE <- bcd$DIS_HEADER_SDATE
+bcs$DIS_HEADR_EDATE <- bcd$DIS_HEADER_SDATE
+bcs$DIS_HEADR_STIME <- bcd$DIS_HEADER_STIME
+bcs$DIS_HEADR_ETIME <- bcd$DIS_HEADER_STIME
+bcs$DIS_HEADR_SLAT <- bcd$DIS_HEADER_SLAT
+bcs$DIS_HEADR_ELAT <- bcd$DIS_HEADER_SLAT
+bcs$DIS_HEADR_SLON <- bcd$DIS_HEADER_SLON
+bcs$DIS_HEADR_ELON <- bcd$DIS_HEADER_SLON
+bcs$DIS_HEADR_START_DEPTH <- bcd$DIS_HEADER_START_DEPTH
+bcs$DIS_HEADR_END_DEPTH <- bcd$DIS_HEADER_END_DEPTH
+bcs$DIS_HEADR_COLLECTOR_SAMPLE_ID <- bcd$DIS_DETAIL_COLLECTOR_SAMP_ID
+
+# filter to unique dis_sample_key_values
+
+bcs <- bcs %>%
+  distinct(DIS_SAMPLE_KEY_VALUE, .keep_all = TRUE)
+
+
+# fill standard values ----
+bcs$MISSION_GEOGRAPHIC_REGION <- 'Davis Strait'
+bcs$DIS_HEADR_TIME_QC_CODE <- 0
+bcs$DIS_HEADR_POSITION_QC_CODE <- 0
+bcs$DIS_HEADR_GEAR_SEQ <- '90000171' # CTD + Rosette
+
+
+# pull values from existing tables ----
+osc_year <- oscdata[oscdata$year == na.omit(unique(raw_data$year)), ]
+
+bcs$MISSION_LEADER <- paste(osc_year$first_name, osc_year$last_name)
+bcs$MISSION_SDATE <- format(osc_year$start_date, '%d-%b-%y')
+bcs$MISSION_EDATE <- format(osc_year$end_date, '%d-%b-%y')
+bcs$MISSION_PLATFORM <- osc_year$platform
+bcs$MISSION_PROTOCOL <- osc_year$protocol
+bcs$DIS_HEADR_RESPONSIBLE_GROUP <- osc_year$responsible_group
+
+# match sounding by event
+event_col <- grep('event', names(raw_data))
+names(raw_data)[event_col] <- 'event'
+
+raw_sounding <- raw_data %>%
+  select(event, sounding) %>%
+  distinct(event, .keep_all = TRUE)
+bcs <- bcs %>%
+  right_join(., raw_sounding, by = join_by( EVENT_COLLECTOR_EVENT_ID == event), relationship = 'many-to-many') %>%
+  rename(., DIS_HEADR_SOUNDING = sounding)
+
+remove(raw_sounding)
+
+# pull event summary
+event_table <- bcd %>%
+  group_by(EVENT_COLLECTOR_EVENT_ID) %>%
+  summarize(EVENT_SDATE = min(DIS_HEADER_SDATE),
+            EVENT_EDATE = max(DIS_HEADER_SDATE),
+            EVENT_STIME = min(DIS_HEADER_STIME),
+            EVENT_ETIME = max(DIS_HEADER_STIME),
+            EVENT_MIN_LAT = min(DIS_HEADER_SLAT),
+            EVENT_MAX_LAT = max(DIS_HEADER_SLAT),
+            EVENT_MIN_LON = min(DIS_HEADER_SLON),
+            EVENT_MAX_LON = max(DIS_HEADER_SLON))
+bcs <- bcs %>%
+  left_join(event_table, by = 'EVENT_COLLECTOR_EVENT_ID') 
+
+# write data_manager_comment 
+# calculation of depth from pressure
+bcs$MISSION_DATA_MANAGER_COMMENT <- "Depth calculated from pressure; start depth equals end depth; sounding manually input;"
+
+# gather comment from raw data
+
+# format & export ----
+bcs_export <- bcs %>%
+  select(any_of(bcs_columns))
+
+bcs_export <- bcs_export[-which(is.na(bcs_export$MISSION_DESCRIPTOR)), ]
 
 # export
+bcs_name <- file.path('../data/BioChem/', paste0(unique(bcs_export$MISSION_DESCRIPTOR), '_BCS.csv'))
+write.csv(bcs_export, bcs_name, row.names = FALSE)
+
