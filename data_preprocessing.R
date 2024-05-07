1d0b86643ffa74279b19bf08b61f1b960008e2b0
EOGrady21 <38440373+EOGrady21@users.noreply.github.com>
2024-05-07 14:20:22 -0300

    outline for preprocessing check to remove issues in raw data

---
+++ data_preprocessing.R
@@ -0,0 +1,11 @@
+# Davis Strait raw data checks
+
+# check for all required metadata columns
+
+# check for empty data columns
+
+# check for entries in reference tables eg expos.csv
+
+# check for unique sample IDs
+
+# check times
